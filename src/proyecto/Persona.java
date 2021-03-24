cd746340e8495e437b2868e25abc2170b71c2b48
Benjamín <benja5022@gmail.com>
2021-03-24 20:32:02 -0300

    Nuevos Cambios y comentarios

---
+++ src/proyecto/Persona.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author Ceseo
+ */
+public class Persona {
+    
+    private String nombre;
+    private String laborProfecional;
+    private int sueldo;
+    private int rut;
+    //private int maquinaria
+
+    public Persona() {
+    }
+
+    
+    public Persona(String nombre, String laborProfecional, int sueldo, int rut) {
+        this.nombre = nombre;
+        this.laborProfecional = laborProfecional;
+        this.sueldo = sueldo;
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLaborProfecional() {
+        return laborProfecional;
+    }
+
+    public void setLaborProfecional(String laborProfecional) {
+        this.laborProfecional = laborProfecional;
+    }
+
+    public int getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(int sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+    
+    
+    
+}
