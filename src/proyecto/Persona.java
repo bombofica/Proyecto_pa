1ebdc8511c53f3b7551a4519b55b0453c952fa7d
Ceseo <Ceseo@DESKTOP-FPH31SD>
2021-03-24 18:57:40 -0300

    un saludo

---
+++ src/proyecto/Persona.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto;
+
+/**
+ *
+ * @author Ceseo
+ */
+public class Persona {
+    
+    private String nombre;
+    private String laborProfecional;
+    private String maquinariaUtilizable;
+    private int sueldo;
+    private int rut;
+
+    public Persona() {
+    }
+
+    
+    public Persona(String nombre, String laborProfecional, String maquinariaUtilizable, int sueldo, int rut) {
+        this.nombre = nombre;
+        this.laborProfecional = laborProfecional;
+        this.maquinariaUtilizable = maquinariaUtilizable;
+        this.sueldo = sueldo;
+        this.rut = rut;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getLaborProfecional() {
+        return laborProfecional;
+    }
+
+    public void setLaborProfecional(String laborProfecional) {
+        this.laborProfecional = laborProfecional;
+    }
+
+    public String getMaquinariaUtilizable() {
+        return maquinariaUtilizable;
+    }
+
+    public void setMaquinariaUtilizable(String maquinariaUtilizable) {
+        this.maquinariaUtilizable = maquinariaUtilizable;
+    }
+
+    public int getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(int sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    public int getRut() {
+        return rut;
+    }
+
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+    
+    
+    
+}
