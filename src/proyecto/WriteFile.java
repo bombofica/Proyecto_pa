acc63f84dcbbc7b0309c255f7b71d44991141648
Benjamín <benja5022@gmail.com>
2021-04-22 17:39:13 -0400

    Se agregó: 1) Limpia pantalla 2) comentarios en WriteFile

--- src/proyecto/WriteFile.java
+++ src/proyecto/WriteFile.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.File;
 
 public class WriteFile {
-
+    // Este método solo se creó para probar el funcionamiento de "FileWriter" 
     public static void escribirFichero(int valor) throws IOException {
         try (FileWriter Escritor = new FileWriter("D:\\Escritorio Real\\ProyectoPA\\proyecto\\proyecto\\src\\proyecto\\pepito.txt")) {
             for (int i = 0; i < valor; i++) {
@@ -17,10 +17,19 @@ public class WriteFile {
         }
     }
     
+    /*Este método se encarga de escribir en "RegistroObras.txt" donde se imprimirá los datos correspondientes al a obra en el siguiente formato:
+      Nombre_Obra,Lugar,Presupuesto_estimado,Tiempo_estimado,
+ 
+        Luego de escribir en "RegistroObras.txt", imprime en las direcciones "RegistroObras/Nombre_obra/empleados.txt" correspondiente a cada Obra
+        donde se imprimen los datos correspondientes a cada empleado
+    */
     public static void escribirObras(char separador, RegistroObras registroObras) throws IOException{
                 
         try (FileWriter Escritor = new FileWriter("RegistroObras//RegistroObras.txt")) { 
-
+            
+            
+            /*Imprime en "RegistroObras.txt" los datos correspondientes a cada obra que se encuentra en un Objeto de tipo "RegistroObras",
+                más específicamente en el ArrayList de RegistroObras*/ 
             Obra current;
             for(int i =0; i < registroObras.contadorObras;i++){
                 current=registroObras.retornarObra(i);
