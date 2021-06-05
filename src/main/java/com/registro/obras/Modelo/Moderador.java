3d0d7ce0c06e971a8bd5ae2724281663c08b4dfb
Benjamín <benja5022@gmail.com>
2021-06-05 16:02:52 -0400

    Intentando hacer encapsulamiento

--- src/main/java/com/registro/obras/Modelo/Moderador.java
+++ src/main/java/com/registro/obras/Modelo/Moderador.java
@@ -27,9 +27,6 @@ public class Moderador extends Persona{
     }
 
     
-    
-    
-    
     @Override
     public String descripcion() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
